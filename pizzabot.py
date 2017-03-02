082ede630ee4708c2c30ef0df9188b8629efd2d1
aandreev <aandreev@usc.edu>
2017-03-02 14:27:44 -0800

    tweet pizza

--- pizzabot.py
+++ pizzabot.py
@@ -7,4 +7,4 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-api.update_status('I am alive!11')
+api.update_status('🍕')
